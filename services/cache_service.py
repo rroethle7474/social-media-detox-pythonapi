edbb05a5e345b9613f3717f6af1bfc6751a891a4
Ryan Roethle <ryanroethle7474@gmail.com>
2025-01-15 21:10:09 -0600

    Updated cache service to fix health endpoint error.

--- services/cache_service.py
+++ services/cache_service.py
@@ -1,11 +1,22 @@
 from cachetools import TTLCache
+from datetime import datetime
 
 class CacheService:
-    def __init__(self):
-        self.cache = TTLCache(maxsize=100, ttl=3600)  # 3600 seconds = 1 hour
+    def __init__(self, maxsize=100, ttl=3600):  # 1 hour default TTL
+        self.cache = TTLCache(maxsize=maxsize, ttl=ttl)
+        self.stats = {
+            'hits': 0,
+            'misses': 0,
+            'last_cleared': None
+        }
 
     def get(self, key):
-        return self.cache.get(key)
+        value = self.cache.get(key)
+        if value is not None:
+            self.stats['hits'] += 1
+        else:
+            self.stats['misses'] += 1
+        return value
 
     def set(self, key, value):
         self.cache[key] = value
@@ -15,7 +26,16 @@ class CacheService:
 
     def clear(self):
         self.cache.clear()
+        self.stats['last_cleared'] = datetime.utcnow()
 
     def remove(self, key):
         if key in self.cache:
             del self.cache[key]
+
+    def get_stats(self):
+        return {
+            **self.stats,
+            'current_size': len(self.cache),
+            'max_size': self.cache.maxsize,
+            'ttl': self.cache.ttl
+        }
